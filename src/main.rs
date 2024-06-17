050689322a5816a2b129e81ff19769a9678e5d27
sircastico <carlosemanuelslb2@gmail.com>
2024-06-17 21:59:20 +0100

    move renderer out of render loop funcs

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use camera::{perspective::Perspective, Camera};
 use images::{image_rgb::{self, ImageRGB}, image_ppm::ImagePPM};
 use lights::{Light, AmbientLight};
 use minifb::{Key, Window, WindowOptions};
+use render::standard_render;
 use scene::Scene;
 use shaders::Shader;
 use utils::{rgb::RGB, vector::{Point, Vector}, Extent2D};
@@ -72,6 +73,9 @@ fn main() {
         continue_prob: 0.5,
     };
 
+
+    let renderer = IncrementalRenderer::new(1, Some(2048), true);
+
     let mut window = Window::new(
         "yep", 
         width as usize, 
@@ -81,18 +85,39 @@ fn main() {
 
     window.set_target_fps(60);
 
-    render_loop_with_swapchain(camera, scene, shader, window, width, height);
+    render_loop_with_swapchain(camera, scene, shader, window, width, height, renderer);
+    //render_loop_sequential(camera, scene, shader, window, width, height, 128, true);
 }
 
-
-fn render_loop<C,S>(camera: C, scene: Scene, shader: S, mut window: Window, width: u32, height: u32)
+fn render_loop_sequential<C,S>(camera: C, scene: Scene, shader: S, mut window: Window, width: u32, height: u32, spp: usize, jitter: bool)
     where
         C: Camera + std::marker::Sync,
         S: Shader + std::marker::Sync,
 {
 
     let mut image = ImageRGB::new(height, width);
-    let mut renderer = IncrementalRenderer::new(1, None, true);
+
+    let mut buf: Vec<u32> = std::iter::repeat(0).take((width*height) as usize).collect();
+
+    standard_render(&camera, &scene, &shader, &mut image, spp, jitter);
+    image.write_to_0rgb_u32(&mut buf, image_rgb::tonemap_reinhard);
+
+    window
+        .update_with_buffer(&buf, width as usize, height as usize)
+        .unwrap();
+
+    while window.is_open() && !window.is_key_down(Key::Escape){
+        window.update();
+    }
+}
+
+fn render_loop<C,S>(camera: C, scene: Scene, shader: S, mut window: Window, width: u32, height: u32, mut renderer: IncrementalRenderer)
+    where
+        C: Camera + std::marker::Sync,
+        S: Shader + std::marker::Sync,
+{
+
+    let mut image = ImageRGB::new(height, width);
 
     let mut buf: Vec<u32> = std::iter::repeat(0).take((width*height) as usize).collect();
     let mut frame_number: u64 = 0;
@@ -112,7 +137,7 @@ fn render_loop<C,S>(camera: C, scene: Scene, shader: S, mut window: Window, widt
     }
 }
 
-fn render_loop_with_swapchain<C,S>(camera: C, scene: Scene, shader: S, mut window: Window, width: u32, height: u32)
+fn render_loop_with_swapchain<C,S>(camera: C, scene: Scene, shader: S, mut window: Window, width: u32, height: u32, mut renderer: IncrementalRenderer)
     where
         C: Camera + std::marker::Sync,
         S: Shader + std::marker::Sync,
@@ -127,9 +152,9 @@ fn render_loop_with_swapchain<C,S>(camera: C, scene: Scene, shader: S, mut windo
     thread::scope(|s|{
         s.spawn(||{
             let mut image = ImageRGB::new(height, width);
-            let mut renderer = IncrementalRenderer::new(1, Some(2048), true);
 
             while !renderer.has_finished() && render_continue.load(Ordering::Relaxed){
+                let inst = Instant::now();
                 renderer.render(&camera, &scene, &shader, &mut image);
 
                 swpchain.update_back(|b|{
@@ -137,6 +162,7 @@ fn render_loop_with_swapchain<C,S>(camera: C, scene: Scene, shader: S, mut windo
                 });
 
                 frame_number.fetch_add(1, Ordering::Relaxed);
+                print!("(frame_gen: {} | gen: {} ms) ", frame_number.load(Ordering::Relaxed), inst.elapsed().as_millis());
             }
             render_finished.store(true, Ordering::Relaxed);
         });
@@ -145,9 +171,11 @@ fn render_loop_with_swapchain<C,S>(camera: C, scene: Scene, shader: S, mut windo
             if !render_finished.load(Ordering::Relaxed){
                 let inst = Instant::now();
                 swpchain.wait_use_front(|buffer|{
+                    let upd_inst = Instant::now();
                     window
                         .update_with_buffer(buffer, width as usize, height as usize)
                         .unwrap();
+                    print!("upd: {} micros | ", upd_inst.elapsed().as_micros());
                 });
                 println!("frame: {} | elapsed: {} ms", frame_number.load(Ordering::Relaxed), inst.elapsed().as_millis());
             } else {
